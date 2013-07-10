2db251b381ac71ac3d7694b4889ca77315ec5b8c
Xavier Lacot <xavier@lacot.org>
2013-07-10 11:46:10 +0200

    added the default back button

--- controllers/page/backButton.js
+++ controllers/page/backButton.js
@@ -1,7 +1,7 @@
 var properties = arguments[0];
 
 $.button.left = properties.properties.left || 0;
-$.button.backgroundImage = properties.properties.backgroundImage || '/images/button/back.png';
+$.button.backgroundImage = properties.properties.backgroundImage || WPATH('images/button/back.png');
 $.button.title = properties.properties.title || L('back');
 $.button.addEventListener('click', function(e) {
     properties.pageflow.back();
