2c437d0918d42bc7c410c780bab128c0f625fe9c
Xavier Lacot <xavier@lacot.org>
2013-08-30 12:18:33 +0200

    do not force left position to be zero

--- controllers/page/backButton.js
+++ controllers/page/backButton.js
@@ -1,6 +1,5 @@
 var properties = arguments[0];
 
-$.button.left = properties.properties.left || 0;
 $.button.backgroundImage = properties.properties.backgroundImage || WPATH('images/button/back.png');
 $.button.title = properties.properties.title || L('back');
 $.button.addEventListener('click', function(e) {
