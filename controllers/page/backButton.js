032e2a9df9d34b9b42a9eb6a2645d22afb4c38a5
Benjamin Clay <bclay@jolicode.com>
2013-10-02 16:09:28 +0200

    We can override the back button's callback

--- controllers/page/backButton.js
+++ controllers/page/backButton.js
@@ -2,6 +2,6 @@ var properties = arguments[0];
 
 $.button.backgroundImage = properties.properties.backgroundImage;
 $.button.title = properties.properties.title || L('back');
-$.button.addEventListener('click', function(e) {
+$.button.addEventListener('click', properties.properties.callback || function(e) {
     properties.pageflow.back();
 });
