5cb535957a824ff8337d08f14326a8fb5f5d1417
Claire Coloma <ccoloma@jolicode.com>
2014-02-10 18:52:42 +0100

    set the backgroundImage property only if it is available

--- controllers/page/backButton.js
+++ controllers/page/backButton.js
@@ -1,6 +1,9 @@
 var properties = arguments[0];
 
-$.button.backgroundImage = properties.properties.backgroundImage;
+if (properties.properties.backgroundImage) {
+    $.button.backgroundImage = properties.properties.backgroundImage;
+}
+
 $.button.title = properties.properties.title || L('back');
 $.button.addEventListener('click', properties.properties.callback || function(e) {
     properties.pageflow.back();
