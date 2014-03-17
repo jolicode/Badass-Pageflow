31eea8141963ca92f658d0338eccf7c2b4001572
zomfg <sergio.kunats@gmail.com>
2014-03-17 15:41:29 +0100

    back button backgroundImage fix

--- controllers/page/backButton.js
+++ controllers/page/backButton.js
@@ -1,6 +1,7 @@
 var properties = arguments[0];
 
-$.button.backgroundImage = properties.properties.backgroundImage;
+if (!!properties && !!properties.properties && !!properties.properties.backgroundImage)
+  $.button.backgroundImage = properties.properties.backgroundImage;
 $.button.title = properties.properties.title || L('back');
 $.button.addEventListener('click', properties.properties.callback || function(e) {
     properties.pageflow.back();
