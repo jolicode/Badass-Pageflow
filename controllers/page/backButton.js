51b6756ccb09a585e0bf02f3b47a379f1bb5e98d
Sylvain Nironi <sylvainnironi@iMac-de-Maxime-2.local>
2014-03-19 14:46:30 +0100

    deleting back title

--- controllers/page/backButton.js
+++ controllers/page/backButton.js
@@ -2,7 +2,7 @@ var properties = arguments[0];
 
 if (!!properties && !!properties.properties && !!properties.properties.backgroundImage)
   $.button.backgroundImage = properties.properties.backgroundImage;
-$.button.title = properties.properties.title || L('back');
+$.button.title = properties.properties.title;
 $.button.addEventListener('click', properties.properties.callback || function(e) {
     properties.pageflow.back();
 });
