32b874a5a73f9532201212fc1248a6221da0370f
Xavier Lacot <xavier@lacot.org>
2015-01-19 17:45:06 +0100

    Made the back button more tweakable

--- controllers/page/backButton.js
+++ controllers/page/backButton.js
@@ -1,8 +1,8 @@
 var properties = arguments[0];
 
-if (properties.properties.backgroundImage) {
-    $.button.backgroundImage = properties.properties.backgroundImage;
-}
+_.each(properties.properties, function(value, key) {
+    $.button[key] = value;
+});
 
 $.button.title = properties.properties.title || L('back');
 $.button.addEventListener('click', properties.properties.callback || function(e) {
